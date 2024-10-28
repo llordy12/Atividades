3cfcccd49051743437e08c4809c508ebabe18dcd
Hector Silva Santos <promotoraeliana@yahoo.com.br>
2024-10-28 13:09:07 -0300

    atividades finalizadas

---
+++ src/atividades_dia_28/LeiaOsNumeros.java
@@ -0,0 +1,30 @@
+package atividades_dia_28;
+
+import java.util.Scanner;
+
+public class LeiaOsNumeros {
+	public static void main(String[] args) {
+		
+		
+		Scanner ler = new Scanner(System.in);
+		
+		
+		float n1;
+		float n2;
+		float n3;
+		float n4;
+		
+		
+		System.out.println("Digite os numeros: ");
+		n1 = ler.nextFloat();
+		n2 = ler.nextFloat();
+		n3 = ler.nextFloat();
+		n4 = ler.nextFloat();
+		
+		System.out.println("Diferenças entre os numeros é:  " + ((n1 * n2)- (n3 * n4)));
+		
+		
+		
+	}
+
+}
--- src/atividades_dia_28/Salario_com_descontos.java
+++ src/atividades_dia_28/Salario_com_descontos.java
@@ -1,5 +1,39 @@
 package atividades_dia_28;
 
+import java.util.Scanner;
+
 public class Salario_com_descontos {
+	public static void main(String[] args) {
+		
+		
+		Scanner ler = new Scanner(System.in);
+		
+		
+		float SalarioBruto;
+		float AdicionalNoturno;
+		float HorasExtras;
+		float Descontos;
+		
+		
+		System.out.println("Digite seu Salario Bruto: ");
+		SalarioBruto = ler.nextFloat();
+		
+		System.out.println("Adicional Noturno: ");
+		AdicionalNoturno = ler.nextFloat();
+		
+		
+		System.out.println("Valor recebido de Horas Extras: ");
+		HorasExtras = ler.nextFloat();
+		
+		
+		System.out.println("Valores descontados: ");
+		Descontos = ler.nextFloat();
+		
+		System.out.println("Seu Salario Liquido é de: " + (SalarioBruto + AdicionalNoturno + (HorasExtras * 5) - Descontos));
+		
+		
+		
+		
+	}
 
 }
