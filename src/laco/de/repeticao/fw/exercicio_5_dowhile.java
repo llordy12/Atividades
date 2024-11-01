6d140a726e06b5e76028b4a260f30e327c6b8711
Hector Silva Santos <promotoraeliana@yahoo.com.br>
2024-11-01 16:09:04 -0300

    atividade 1 vetor

--- src/laco/de/repeticao/fw/exercicio_5_dowhile.java
+++ src/laco/de/repeticao/fw/exercicio_5_dowhile.java
@@ -23,3 +23,4 @@ public class exercicio_5_dowhile {
         scanner.close();
     }
 }
+   
---
+++ src/vetor/Ex_1_vetor.java
@@ -0,0 +1,43 @@
+package vetor;
+
+import java.util.Scanner;
+
+public class Ex_1_vetor {
+
+	public static void main(String[] args) {
+        
+        int[] vetor = {2, 5, 1, 3, 4, 9, 7, 8, 10, 6};
+
+       
+        System.out.println("Vetor:");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println(i + ": " + vetor[i]);
+        }
+
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o número que voce deseja encontrar: ");
+        int numProcurado = scanner.nextInt();
+
+        
+        int posicao = -1;
+
+  
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] == numProcurado) {
+                posicao = i;
+               
+            }
+        }
+
+        // Exibe o resultado
+        if (posicao != -1) {
+            System.out.println("O numero " + numProcurado + " esta na posição: " + posicao);
+        } else {
+            System.out.println("O numero " + numProcurado + " não foi encontrado!");
+        }
+
+        scanner.close(); // Fecha o scanner
+    }
+
+}
